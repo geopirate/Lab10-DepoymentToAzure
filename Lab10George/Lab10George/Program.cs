7c31649e8f30d52c338a1ab148991903ae6ef44a
George Gaylor <georgegaylor83@outlook.com>
2017-09-25 23:39:38 -0700

    Page work done with some styling.

--- Lab10George/Lab10George/Program.cs
+++ Lab10George/Lab10George/Program.cs
@@ -14,12 +14,16 @@ namespace Lab10George
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://localhost:5000")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseApplicationInsights()
                 .Build();
+
+            host.Run();
+        }
     }
 }
---
+++ Lab10George/Lab10George/readme.md
@@ -0,0 +1,8 @@
+﻿<h2>How long did it take you to complete this assignment?</h2>
+<p> This took about an hour, mostly wrestling with Azure.</p>
+<h2>What did you struggle with? Why? How did you solve?</h2>
+<p>	Mostly struggled getting the azure subscription working, I dug through the settings/help to solve it.</p>
+<h2>What did you learn during this assignment?</h2>
+<p>	Just looking at the different features in Azure and getting something deployed now.</p>
+<h2>What resources did you utilize for this assingment?</h2>
+<p>	The docs and some tinkering as well as some previous projects.</p>
